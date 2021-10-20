8581a1a6d440095117f80efc1d535cddb5f4a545
vis-alex <usenkoshurik@mail.ru>
2021-10-20 17:17:17 +0300

    7.7 jdbc validation patch

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -48,7 +48,7 @@ public class Meal extends AbstractBaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @NotNull
+//    @NotNull
     private User user;
 
     public Meal() {
