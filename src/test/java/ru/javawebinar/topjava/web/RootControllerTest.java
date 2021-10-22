11ea6f61b343194d90c31a8ec80e6ac764784e0a
vis-alex <usenkoshurik@mail.ru>
2021-10-22 21:47:36 +0300

    HW7 Add getMeals test

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,7 +2,10 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
@@ -25,6 +28,24 @@ class RootControllerTest extends AbstractControllerTest {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
                                 MATCHER.assertMatch(actual, admin, user);
+                            }
+                        }
+                ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<MealTo>>() {
+
+                            @Override
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                MealTestData.MEAL_TO_MATCHER.assertMatch(actual, MealsUtil.getTos(MealTestData.meals, MealsUtil.DEFAULT_CALORIES_PER_DAY));
                             }
                         }
                 ));
